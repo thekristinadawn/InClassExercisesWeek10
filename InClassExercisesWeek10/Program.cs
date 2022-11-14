6bdb78e4da8acd1533ac88bf2ad698549f906b8a
Kristina Palmer <thekristinadawn@gmail.com>
2022-11-14 15:07:22 -0600

    First Update

--- InClassExercisesWeek10/Program.cs
+++ InClassExercisesWeek10/Program.cs
@@ -10,6 +10,27 @@ namespace InClassExercisesWeek10
     {
         static void Main(string[] args)
         {
+            //create dictionary
+
+            Dictionary<String, List<string>> inventory = new Dictionary<string, List<string>>();
+
+            List<string> list1 = new List<string>();
+            list1.Add("500");
+
+            List<string> list2 = new List<string>();
+            list2.Add("seashell"); list2.Add("strange berry"); list2.Add("lint");
+
+            inventory["gold"] = list1;
+            inventory["pouch"] = list2;
+
+            foreach (string key in inventory.Keys)
+            {
+                foreach (string value in inventory[key])
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
         }
     }
 }
